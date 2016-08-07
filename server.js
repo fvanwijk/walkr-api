003b8a2dcb9a646f477e96d39dc08dacc0bb55fe
Frank van Wijk <fvanwijk@xebia.com>
2016-08-07 13:19:10 +0200

    Filter out private fields

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ var express = require('express');
 var app = express();
 var router = require('./router');
 var mongoose   = require('mongoose');
+var mongoosemask = require('mongoosemask');
 var url = require('./api/url');
 
 mongoose.connect('mongodb://localhost:27017');
+
+app.use(mongoosemask(['_id', '__v']));
 app.use('/api', router);
 app.listen(url.port);
 console.log(`Running Walkr API on port ${url.port}`);
