b7f52a6e4983d6e48f447be4cb7914f7c560a7e0
Frank van Wijk <fvanwijk@xebia.com>
2016-08-07 12:48:08 +0200

    Get port and host data from url file

---
+++ api/url.js
@@ -0,0 +1,4 @@
+module.exports = {
+  host: 'http://localhost',
+  port: process.env.PORT || 1337
+};
--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 var router = require('./router');
 var mongoose   = require('mongoose');
-var port = process.env.PORT || 1337;
+var url = require('./api/url');
 
 mongoose.connect('mongodb://localhost:27017');
 app.use('/api', router);
-app.listen(port);
-console.log(`Running Walkr API on port ${port}`);
+app.listen(url.port);
+console.log(`Running Walkr API on port ${url.port}`);
