22a7534233185cb3185675c5e2c5a74d676c2d16
Frank van Wijk <fvanwijk@xebia.com>
2016-08-07 12:26:03 +0200

    Refactor API calls into common-api

--- api/common-api.js
+++ api/common-api.js
@@ -15,5 +15,40 @@ module.exports = {
         });
       });
     }
+  },
+  postAll: function (Model, filename) {
+    return function (req, res) {
+      Model.remove({});
+      var items = require(`../data/${filename}`);
+      items.forEach(item => {
+        var model = new Model(item);
+        model.save(function (err) {
+          if (err) {
+            res.send(err);
+          }
+        })
+      });
+      res.sendStatus(200);
+    }
+  },
+  deleteAll: function (Model) {
+    return function (req, res) {
+      Model.remove({}, function (err) {
+        if (err) {
+          res.send(err);
+        }
+        res.sendStatus(200);
+      });
+    }
+  },
+  get: function (Model, field, paramName) {
+    return function(req, res) {
+      Model.findOne({ [field]: req.params[paramName] }, function(err, item) {
+        if (err) {
+          res.send(err);
+        }
+        res.json(item);
+      });
+    }
   }
 };
--- api/planets.js
+++ api/planets.js
@@ -5,34 +5,9 @@ var CommonApi = require('./common-api');
 module.exports = function (router) {
   router.route('/planets')
     .get(CommonApi.getAll(Planet))
-    .post(function (req, res) {
-      Planet.remove({});
-      var planets = require('../data/planets.json');
-      planets.forEach(planet => {
-        var model = new Planet(planet);
-        model.save(function (err) {
-          if (err) {
-            res.send(err);
-          }
-        })
-      });
-      res.json({ message: 'Planets created!' });
-    })
-    .delete(function (req, res) {
-      Planet.remove({}, function (err) {
-        if (err) {
-          res.send(err);
-        }
-        res.send(200);
-      });
-    });
+    .post(CommonApi.postAll(Planet, 'planets.json'))
+    .delete(CommonApi.deleteAll(Planet));
 
-  router.route('/planets/:planet_id')
-    .get(function(req, res) {
-      Planet.findById(req.params.planet_id, function(err, planet) {
-        if (err)
-          res.send(err);
-        res.json(planet);
-      });
-    });
+  router.route('/planets/:planet_code')
+    .get(CommonApi.get(Planet, 'code', 'planet_code'));
 };
