33424b6e4bb9c8002a718036ee3a233ea3bed1ab
Frank van Wijk <fvanwijk@xebia.com>
2016-08-07 11:02:40 +0200

    Log every request

--- router.js
+++ router.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(function (req, res, next) {
+  console.log('Receiving request');
+  next();
+});
+
 router.get('/', function (req, res) {
   return res.json({ message: 'Hello world!' });
 });
