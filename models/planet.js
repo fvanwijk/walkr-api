0e06563fdb9a67249f8f6751cc558b2edb397623
Frank van Wijk <fvanwijk@xebia.com>
2016-08-07 12:25:43 +0200

    Enrich planet model with new fields

--- models/planet.js
+++ models/planet.js
@@ -2,7 +2,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var PlanetSchema = new Schema({
-  name: String
+  code: 'number',
+  name: String,
+  type: String,
+  gravity: String,
+  creature: String,
+  resource: String
 });
 
 module.exports = mongoose.model('Planet', PlanetSchema);
