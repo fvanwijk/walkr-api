16499fd8b1d9ce96513a18a64552c67a56ca8cd3
Frank van Wijk <fvanwijk@xebia.com>
2016-08-07 14:26:33 +0200

    Send 404 for all other requests

--- server.js
+++ server.js
@@ -9,5 +9,8 @@ mongoose.connect('mongodb://localhost:27017');
 
 app.use(mongoosemask(['_id', '__v']));
 app.use('/api', router);
+app.get('*', function(req, res){
+  res.sendStatus(404);
+});
 app.listen(url.port);
 console.log(`Running Walkr API on port ${url.port}`);
