e34e6925a71fddbcfb5171806208004cb9a89383
Frank van Wijk <fvanwijk@xebia.com>
2016-11-14 22:07:16 +0100

    Add variant getter that uses catchWrapper

--- api/ships.js
+++ api/ships.js
@@ -13,4 +13,17 @@ module.exports = function (router) {
     .post(CommonApi.post(Ship))
     .put(CommonApi.put(Ship))
     .delete(CommonApi.delete(Ship));
+
+  router.route('/ships/:id/:variant')
+    .get(function (req, res) {
+      Ship.findOne(
+        { id: req.params.id, 'variants.id': req.params.variant },
+        { _id: false },
+        function (err, item) {
+          CommonApi.catchWrapper(err, item, res, function (res, item) {
+            res.json(item.variants.find(variant => { return variant.id === req.params.variant }));
+          });
+        }
+      );
+    })
 };
