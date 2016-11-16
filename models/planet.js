685098e28475fc38fc59f9ac8ed047e2700128fa
Frank van Wijk <fvanwijk@xebia.com>
2016-11-16 20:57:08 +0100

    Add some planet fields for personal API

--- models/planet.js
+++ models/planet.js
@@ -5,10 +5,19 @@ var PlanetSchema = new Schema({
   url: String,
   code: Number,
   name: String,
+  description: String,
   type: String,
   gravity: String,
   creature: String,
-  resource: String
+  creature_description: String,
+  resource: String,
+  level: Number,
+  discovery_date: Date,
+  base_price: { name: String, quantity: Number },
+  next_upgrade_price: { name: String, quantity: Number },
+  requirements: { name: String, quantity: Number },
+  resource_value: { name: String, quantity: Number },
+  completion_time: Number // minutes
 });
 
 var planet = mongoose.model('Planet', PlanetSchema);
