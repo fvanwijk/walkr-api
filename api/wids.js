4fb6af8a4cde2b094fc986d453054e7590dd6669
Frank van Wijk <fvanwijk@xebia.com>
2016-11-20 12:28:11 +0100

    Only show URLs in WIDs list

--- api/wids.js
+++ api/wids.js
@@ -1,6 +1,7 @@
 var router = require('../router');
 var WID = require('../models/wid').model;
 var Discovery = require('../models/discovery').model;
+var DFRDiscovery = require('../models/dfr_discovery').model;
 var CommonApi = require('./common-api');
 var url = require('./url');
 
@@ -11,7 +12,11 @@ module.exports = function (router) {
     .delete(CommonApi.deleteAll(WID));
 
   router.route('/wids/:id')
-    .get(CommonApi.get(WID, CommonApi.projectionMapper(Object.keys(Discovery.schema.tree), ['url', 'wid', 'planet'], 'planets')))
+    .get(CommonApi.get(WID, Object.assign(
+        CommonApi.projectionMapper(Object.keys(Discovery.schema.tree), ['url', 'wid', 'planet'], 'planets'),
+        CommonApi.projectionMapper(Object.keys(DFRDiscovery.schema.tree), ['url', 'wid', 'dfr'], 'dfrs')
+      )
+    ))
     .post(CommonApi.post(WID))
     .put(CommonApi.put(WID))
     .delete(CommonApi.delete(WID));
