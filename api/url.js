b18efee0073cf906f05bd252de67a746b3a1e556
Frank van Wijk <fvanwijk@xebia.com>
2017-03-15 21:35:10 +0100

    Fix URL creation by converting the arguments to string

--- api/url.js
+++ api/url.js
@@ -4,6 +4,6 @@ module.exports = {
   host: 'http://localhost',
   port: process.env.PORT || 1337,
   create() {
-    return `${this.host}:${this.port}` + path.join.apply(this, ['/api'].concat(Array.prototype.slice.call(arguments)));
+    return `${this.host}:${this.port}` + path.join.apply(this, ['/api', ...Array.prototype.slice.call(arguments).map(arg => arg.toString())]);
   }
 };
