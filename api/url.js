45ed7e732c9293058c86c677f0943d269f0b2c10
Frank van Wijk <fvanwijk@xebia.com>
2017-09-09 13:43:41 +0200

    Omit port in REST URLs on production

--- api/url.js
+++ api/url.js
@@ -1,13 +1,14 @@
+const config = require(`../config/${process.env === 'production' ? 'production' : 'develop'}`);
 const path = require('path');
 
 module.exports = {
-  host: 'http://localhost',
-  port: process.env.PORT || 1337,
+  host: config.webHostname,
+  port: process.env.PORT || config.webPort,
   create() {
-    return `${this.host}:${this.port}` + path.join.apply(this, ['/api', ...Array.prototype.slice.call(arguments).map(arg => arg.toString())]);
+    return `${this.host}${config.webPort ? ':' + config.webPort : ''}` + path.join.apply(this, ['/api', ...Array.prototype.slice.call(arguments).map(arg => arg.toString())]);
   },
   addHostToUrl(item) {
-    item.url = `${this.host}:${this.port}${item.url}`;
+    item.url = `${this.host}${config.webPort ? ':' + config.webPort : ''}${item.url}`;
     return item;
   }
 };
