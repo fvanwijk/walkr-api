fb1e4b1f58f95d1046a1f70114f30225f6307903
Frank van Wijk <fvanwijk@xebia.com>
2017-09-09 12:34:39 +0200

    Environment specific connection and better error handling

--- server.js
+++ server.js
@@ -4,15 +4,22 @@ var router = require('./router');
 var mongoose   = require('mongoose');
 var bodyParser = require('body-parser');
 var url = require('./api/url');
+var config = require(`./config/${process.env.NODE_ENV === 'production' ? 'production' : 'develop'}.json`);
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.NODE_ENV === 'production' ? process.env.MONGOLAB_URI : 'mongodb://localhost:27017');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use('/api', router);
-app.get('*', function(req, res){
-  res.sendStatus(404);
+mongoose.connect(config.mongoUrl, {
+  user: process.env.MONGOLAB_USER,
+  pass: process.env.MONGOLAB_PASSWORD,
+}).then(() => {
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use('/api', router);
+  app.get('*', function(req, res){
+    res.sendStatus(404);
+  });
+  app.listen(url.port);
+  console.info(`Connecting with Mongo database on ${config.mongoUrl}`);
+  console.info(`Running Walkr API on port ${url.port}`);
+}, () => {
+  console.error(`Could not connect with Mongo database on ${config.mongoUrl}`);
 });
-app.listen(url.port);
-console.log(`Running Walkr API on port ${url.port}`);
