291d4b2d0d3610b25402cd815142cb25602021e8
Frank van Wijk <fvanwijk@xebia.com>
2017-09-09 13:03:53 +0200

    Clean up epics output

--- api/epics.js
+++ api/epics.js
@@ -1,5 +1,6 @@
 var router = require('../router');
 var epic = require('../models/epic').model;
+var Planet = require('../models/planet').model;
 var CommonApi = require('./common-api');
 
 module.exports = function (router) {
@@ -8,8 +9,20 @@ module.exports = function (router) {
     .post(CommonApi.postAll(epic, 'epics.json'))
     .delete(CommonApi.deleteAll(epic));
 
+  const projectionMap = Object.assign({
+      'rounds._id': false,
+      'rounds.subrounds._id': false,
+      'rounds.subrounds.donation._id': false,
+      'rounds.choices._id': false,
+      'rounds.choices.rounds._id': false,
+      'rounds.choices.rounds.subrounds._id': false,
+      'rounds.choices.rounds.subrounds.donation._id': false
+    },
+    CommonApi.projectionMapper(Object.keys(Planet.schema.tree), ['url', 'name'], 'rewards.galaxy_map')
+  );
+
   router.route('/epics/:id')
-    .get(CommonApi.get(epic))
+    .get(CommonApi.get(epic, projectionMap))
     .post(CommonApi.post(epic))
     .put(CommonApi.put(epic))
     .delete(CommonApi.delete(epic));
