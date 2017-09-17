4b63feb3f1410b9da68e85439b2007cd5d7919d0
Frank van Wijk <fvanwijk@xebia.com>
2017-09-17 10:14:01 +0200

    Set extra headers to allow PUT and DELETE method from browser

--- server.js
+++ server.js
@@ -12,8 +12,12 @@ mongoose.connect(config.mongoUrl, {
   pass: process.env.MONGOLAB_PASSWORD,
 }).then(() => {
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    if (req.method === 'OPTIONS') {
+      res.status(200);
+    }
     next();
   });
 
